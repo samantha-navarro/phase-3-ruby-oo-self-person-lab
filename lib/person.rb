8ed2a093696bd642622f7738882c915b7a1daa25
Samantha Navarro <samantha.m.navarro@gmail.com>
2022-06-28 21:56:29 -0700

    commit tree fb4f041077382829ddce503b145484c3ffbb6493

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,28 @@
 # your code goes here
+class Person
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+
+end
+
+    def happiness= (num)
+        @happiness = num
+        @happiness = 10 if @happiness > 10
+        @happiness = 0 if @happiness < 0
+        @happiness = if num > 10
+                        10
+                    elsif num < 0
+                        0
+                    else
+                        num
+                    end
+end
+
+
+
+
+
+
+
+
+
